bbe92a732dbe6d5d2674a101f17e3a087668e224
vjerani <vjivicic@globalnet.hr>
2021-11-05 11:38:43 +0100

    Initial files

---
+++ Core/Entities/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    // This can easily be modified to be BaseEntity<T> and public T Id to support different key types.
+    // Using non-generic integer types for simplicity and to ease caching logic
+    public abstract class BaseEntity
+    {
+        public virtual int Id { get; protected set; }
+    }
+}
